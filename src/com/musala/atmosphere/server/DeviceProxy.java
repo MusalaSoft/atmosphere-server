d93de1483cb3128f549bc44314593424556a66c5
yordan.petrov <yordan.petrov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-08-01 11:39:49 +0000

    git-svn-id: https://trac.musala.com/ARDC/repo@8542 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/server/DeviceProxy.java
+++ src/com/musala/atmosphere/server/DeviceProxy.java
@@ -327,4 +327,20 @@ public class DeviceProxy extends UnicastRemoteObject implements IClientDevice
 			throw new RuntimeException("Connection to device failed.", e);
 		}
 	}
+	
+	public void setAirplaneMode(boolean airplaneMode)
+			throws CommandFailedException,
+			RemoteException
+	{
+		try
+		{
+			wrappedDevice.setAirplaneMode(airplaneMode);
+		}
+		catch (RemoteException e)
+		{
+			// TODO handle remoteexception (the server should know the connection is bad)
+			// and decide what to do next. This next line is temporal.
+			throw new RuntimeException("Connection to device failed", e);
+		}
+	}
 }
