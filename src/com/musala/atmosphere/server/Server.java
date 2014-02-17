33419234ab54179b8c87187e58cae24d30140c96
vladimir.vladimirov <vladimir.vladimirov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-17 16:03:31 +0000

    Initial import:
     * New state on the agent - DisconnectedAgent;
     * Several Unit tests for the following commands: server-address, performance, devices and one Base unit test which all other tests inherit.
    
    Changeset:
    
     * Completely refactored the N. Taushanov state design patter;
     * The result is:
      * 17 removed classes - mostly commands, states of the agents and tests;
      * Refactored the whole command factory;
      * Changed the logic flow when executing a command from the agent;
      * Reduced a lot of duplicated and unnecessary code;
      * Removed the problematic thread that just won't stop working when executing 'exit' command;
     * Added some log messages where I thought it was necessary to refactor the old code;
     * Changed the ConsoleControl to separate string commands to <String, List<String>> instead of <String, String[]> and refactored the Server code a little to keep the things compatible and compilable;
    
    Notes:
    
     * Fixed the bug with the inner thread in the ( none-existing anymore ) RunningState of the agent.
    
    [http://jenkins.musala.com/r/6835/ $6835] see #2730
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9674 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/server/Server.java
+++ src/com/musala/atmosphere/server/Server.java
@@ -3,6 +3,7 @@ package com.musala.atmosphere.server;
 import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -16,216 +17,195 @@ import com.musala.atmosphere.server.state.ServerState;
 import com.musala.atmosphere.server.state.StoppedServer;
 import com.musala.atmosphere.server.util.ServerPropertiesLoader;
 
-public class Server
-{
+public class Server {
 
-	private static final Logger LOGGER = Logger.getLogger(Server.class.getCanonicalName());
+    private static final Logger LOGGER = Logger.getLogger(Server.class.getCanonicalName());
 
-	private ServerManager serverManager;
+    private ServerManager serverManager;
 
-	private ConsoleControl serverConsole;
+    private ConsoleControl serverConsole;
 
-	private ServerCommandFactory commandFactory;
+    private ServerCommandFactory commandFactory;
 
-	private ServerState currentServerState;
+    private ServerState currentServerState;
 
-	private int serverRmiPort;
+    private int serverRmiPort;
 
-	private boolean closed;
+    private boolean closed;
 
-	/**
-	 * Creates a Server component bound on specified in the properties file port.
-	 * 
-	 * @throws RemoteException
-	 */
-	public Server() throws RemoteException
-	{
-		this(ServerPropertiesLoader.getPoolManagerRmiPort());
-	}
+    /**
+     * Creates a Server component bound on specified in the properties file port.
+     * 
+     * @throws RemoteException
+     */
+    public Server() throws RemoteException {
+        this(ServerPropertiesLoader.getPoolManagerRmiPort());
+    }
 
-	/**
-	 * Creates a Server component bound on given port.
-	 * 
-	 * @param serverPort
-	 *        - port on which the Pool Manager of the Server will be published in RMI.
-	 * @throws RemoteException
-	 */
-	public Server(int serverPort) throws RemoteException
-	{
-		serverRmiPort = serverPort;
-		serverManager = new ServerManager(serverRmiPort);
-		setState(new StoppedServer(this));
+    /**
+     * Creates a Server component bound on given port.
+     * 
+     * @param serverPort
+     *        - port on which the Pool Manager of the Server will be published in RMI.
+     * @throws RemoteException
+     */
+    public Server(int serverPort) throws RemoteException {
+        serverRmiPort = serverPort;
+        serverManager = new ServerManager(serverRmiPort);
+        setState(new StoppedServer(this));
 
-		serverConsole = new ConsoleControl();
-		commandFactory = new ServerCommandFactory(this);
+        serverConsole = new ConsoleControl();
+        commandFactory = new ServerCommandFactory(this);
 
-		closed = false;
-		LOGGER.info("Server instance created succesfully.");
-	}
+        closed = false;
+        LOGGER.info("Server instance created succesfully.");
+    }
 
-	/**
-	 * Sets the server state.
-	 * 
-	 * @param newState
-	 */
-	public void setState(ServerState newState)
-	{
-		currentServerState = newState;
-	}
+    /**
+     * Sets the server state.
+     * 
+     * @param newState
+     */
+    public void setState(ServerState newState) {
+        currentServerState = newState;
+    }
 
-	/**
-	 * Starts the Server thread if it is not already running.
-	 */
-	public void run()
-	{
-		currentServerState.run();
-	}
+    /**
+     * Starts the Server thread if it is not already running.
+     */
+    public void run() {
+        currentServerState.run();
+    }
 
-	/**
-	 * Stops the Server if it's running.
-	 */
-	public void stop()
-	{
-		currentServerState.stop();
-	}
+    /**
+     * Stops the Server if it's running.
+     */
+    public void stop() {
+        currentServerState.stop();
+    }
 
-	/**
-	 * Releases all resources used by the server and marks it as closed. After that the Server is no longer available
-	 * and should be started again in order to be used.
-	 */
-	public void exit()
-	{
-		stop();
-		ClientRequestMonitor deviceMonitor = ClientRequestMonitor.getInstance();
-		deviceMonitor.stop();
+    /**
+     * Releases all resources used by the server and marks it as closed. After that the Server is no longer available
+     * and should be started again in order to be used.
+     */
+    public void exit() {
+        stop();
+        ClientRequestMonitor deviceMonitor = ClientRequestMonitor.getInstance();
+        deviceMonitor.stop();
 
-		serverManager.close();
-		closed = true;
-	}
+        serverManager.close();
+        closed = true;
+    }
 
-	/**
-	 * Prints a string to the Server's console output.
-	 * 
-	 * @param message
-	 *        - the message to be printed.
-	 */
-	public void writeToConsole(String message)
-	{
-		currentServerState.writeToConsole(message);
-	}
+    /**
+     * Prints a string to the Server's console output.
+     * 
+     * @param message
+     *        - the message to be printed.
+     */
+    public void writeToConsole(String message) {
+        currentServerState.writeToConsole(message);
+    }
 
-	/**
-	 * Prints a line to the Server's console output.
-	 * 
-	 * @param message
-	 *        - the message to be printed.
-	 */
-	public void writeLineToConsole(String message)
-	{
-		currentServerState.writeLineToConsole(message);
-	}
+    /**
+     * Prints a line to the Server's console output.
+     * 
+     * @param message
+     *        - the message to be printed.
+     */
+    public void writeLineToConsole(String message) {
+        currentServerState.writeLineToConsole(message);
+    }
 
-	/**
-	 * Executes a passed command from the console.
-	 * 
-	 * @param passedShellCommand
-	 *        - the passed shell command.
-	 * @throws IOException
-	 */
-	private void parseAndExecuteShellCommand(String passedShellCommand) throws IOException
-	{
-		if (passedShellCommand != null)
-		{
-			Pair<String, String[]> parsedCommand = ConsoleControl.parseShellCommand(passedShellCommand);
-			String command = parsedCommand.getKey();
-			String[] params = parsedCommand.getValue();
+    /**
+     * Executes a passed command from the console.
+     * 
+     * @param passedShellCommand
+     *        - the passed shell command.
+     * @throws IOException
+     */
+    private void parseAndExecuteShellCommand(String passedShellCommand) throws IOException {
+        if (passedShellCommand != null) {
+            Pair<String, List<String>> parsedCommand = ConsoleControl.parseShellCommand(passedShellCommand);
+            String command = parsedCommand.getKey();
+            List<String> paramsAsList = parsedCommand.getValue();
 
-			if (!command.isEmpty())
-			{
-				executeShellCommand(command, params);
-			}
-		}
-		else
-		{
-			LOGGER.error("Error in console: trying to execute 'null' as a command.");
-			throw new IllegalArgumentException("Command passed to server is 'null'");
-		}
-	}
+            if (!command.isEmpty()) {
+                String[] params = new String[paramsAsList.size()];
+                paramsAsList.toArray(params);
+                executeShellCommand(command, params);
+            }
+        }
+        else {
+            LOGGER.error("Error in console: trying to execute 'null' as a command.");
+            throw new IllegalArgumentException("Command passed to server is 'null'");
+        }
+    }
 
-	/**
-	 * Evaluates a passed command and calls the appropriate Server method.
-	 * 
-	 * @param commandName
-	 *        - command for execution.
-	 * @param params
-	 *        - passed command arguments.
-	 */
-	private void executeShellCommand(String commandName, String[] params)
-	{
-		ServerConsoleCommands command = ServerConsoleCommands.findCommand(commandName);
+    /**
+     * Evaluates a passed command and calls the appropriate Server method.
+     * 
+     * @param commandName
+     *        - command for execution.
+     * @param params
+     *        - passed command arguments.
+     */
+    private void executeShellCommand(String commandName, String[] params) {
+        ServerConsoleCommands command = ServerConsoleCommands.findCommand(commandName);
 
-		if (command == null)
-		{
-			currentServerState.writeLineToConsole("Unknown command. Use 'help' to retrieve list of available commands.");
-			return;
-		}
+        if (command == null) {
+            currentServerState.writeLineToConsole("Unknown command. Use 'help' to retrieve list of available commands.");
+            return;
+        }
 
-		ServerCommand executableCommand = commandFactory.getCommandInstance(command);
-		executableCommand.execute(params);
-	}
+        ServerCommand executableCommand = commandFactory.getCommandInstance(command);
+        executableCommand.execute(params);
+    }
 
-	/**
-	 * Reads one line from the server's console. For more information see
-	 * {@link com.musala.atmosphere.server.ServerConsole#readLine() ServerConsole.readLine()}
-	 * 
-	 * @return the first line in the console buffer as a String.
-	 * @throws IOException
-	 *         - when a console reading error occurs.
-	 */
-	public String readCommandFromConsole() throws IOException
-	{
-		String command = serverConsole.readCommand();
-		return command;
-	}
+    /**
+     * Reads one line from the server's console. For more information see
+     * {@link com.musala.atmosphere.server.ServerConsole#readLine() ServerConsole.readLine()}
+     * 
+     * @return the first line in the console buffer as a String.
+     * @throws IOException
+     *         - when a console reading error occurs.
+     */
+    public String readCommandFromConsole() throws IOException {
+        String command = serverConsole.readCommand();
+        return command;
+    }
 
-	/**
-	 * 
-	 * @return true if the server is closed, false otherwise.
-	 */
-	private boolean isClosed()
-	{
-		return closed;
-	}
+    /**
+     * 
+     * @return true if the server is closed, false otherwise.
+     */
+    private boolean isClosed() {
+        return closed;
+    }
 
-	public static void main(String[] args) throws NotBoundException, IOException, InterruptedException
-	{
-		// Check if an argument which specifies a port for the Server was passed.
-		int portToCreateServerOn = 0;
-		try
-		{
-			if (args.length == 1)
-			{
-				String passedRmiPort = args[0];
-				portToCreateServerOn = Integer.parseInt(passedRmiPort);
-			}
-			else
-			{
-				portToCreateServerOn = ServerPropertiesLoader.getPoolManagerRmiPort();
-			}
-		}
-		catch (NumberFormatException e)
-		{
-			String errorMessage = "Parsing passed port resulted in an exception.";
-			LOGGER.fatal(errorMessage, e);
-			throw new RuntimeException(errorMessage, e);
-		}
+    public static void main(String[] args) throws NotBoundException, IOException, InterruptedException {
+        // Check if an argument which specifies a port for the Server was passed.
+        int portToCreateServerOn = 0;
+        try {
+            if (args.length == 1) {
+                String passedRmiPort = args[0];
+                portToCreateServerOn = Integer.parseInt(passedRmiPort);
+            }
+            else {
+                portToCreateServerOn = ServerPropertiesLoader.getPoolManagerRmiPort();
+            }
+        }
+        catch (NumberFormatException e) {
+            String errorMessage = "Parsing passed port resulted in an exception.";
+            LOGGER.fatal(errorMessage, e);
+            throw new RuntimeException(errorMessage, e);
+        }
 
-		Server localServer = new Server(portToCreateServerOn);
-		localServer.run();
-		do
-		{
-			String passedShellCommand = localServer.readCommandFromConsole();
-			localServer.parseAndExecuteShellCommand(passedShellCommand);
-		} while (!localServer.isClosed());
-	}
+        Server localServer = new Server(portToCreateServerOn);
+        localServer.run();
+        do {
+            String passedShellCommand = localServer.readCommandFromConsole();
+            localServer.parseAndExecuteShellCommand(passedShellCommand);
+        } while (!localServer.isClosed());
+    }
 }
