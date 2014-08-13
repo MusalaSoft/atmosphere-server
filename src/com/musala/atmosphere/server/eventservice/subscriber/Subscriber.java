a1b775af632fc82a3cbe44be816919772a965055
filareta.yordanova <filareta.yordanova@34708651-a5da-c34d-b20c-10ddab38317d>
2014-08-13 06:26:36 +0000

    Fix links in javadoc.
    git-svn-id: https://trac.musala.com/ARDC/repo@10392 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/server/eventservice/subscriber/Subscriber.java
+++ src/com/musala/atmosphere/server/eventservice/subscriber/Subscriber.java
@@ -1,11 +1,9 @@
 package com.musala.atmosphere.server.eventservice.subscriber;
 
-import com.musala.atmosphere.server.eventservice.ServerEventService;
 import com.musala.atmosphere.server.eventservice.event.Event;
 
 /**
- * Defines an interface for all objects, that can subscribe to an {@link ServerEventService event service} for receiving
- * events.
+ * Defines an interface for all objects, that can subscribe to an event service for receiving events.
  * 
  * @author filareta.yordanova
  * 
@@ -13,7 +11,7 @@ import com.musala.atmosphere.server.eventservice.event.Event;
 public interface Subscriber {
     /**
      * Informs the subscriber when an {@link Event event}, for which the object is subscribed, is received from the
-     * {@link ServerEventService event service}.
+     * event service.
      * 
      * @param event
      *        - event, transferred by the event service.
