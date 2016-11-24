0fbbe59e6439401ecb2593d1d2fedae2a6a17537
Aleksander Ivanov <aleksander.ivanov@musala.com>
2016-11-24 17:28:44 +0200

    Make log messages more user-friendly
    Omit class names from stdout log and display a "has started successfully" message when finished loading.

--- src/main/java/com/musala/atmosphere/server/state/RunningServer.java
+++ src/main/java/com/musala/atmosphere/server/state/RunningServer.java
@@ -8,9 +8,9 @@ import com.musala.atmosphere.server.Server;
 /**
  * This class represents the running state of the Server. It creates a new inner thread which prevents the Server from
  * closing until explicitly stated so.
- * 
+ *
  * @author vladimir.vladimirov
- * 
+ *
  */
 public class RunningServer extends ServerState {
     private static final Logger LOGGER = Logger.getLogger(RunningServer.class.getCanonicalName());
@@ -45,6 +45,8 @@ public class RunningServer extends ServerState {
         @Override
         public void run() {
             LOGGER.info("Running Server...");
+            String serverStartedMessage = "The Server has started successfully.";
+            writeLineToConsole(serverStartedMessage);
             while (isRunning) {
                 try {
                     // we must make sure the server thread is not stopped until we say so
