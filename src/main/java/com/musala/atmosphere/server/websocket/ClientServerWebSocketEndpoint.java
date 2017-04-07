7948ff89df4483b076b1fc7a4e7358f443421ae1
Alexander Ivanov <aleksander.ivanov@musala.com>
2017-04-07 17:30:08 +0300

    Introduce workaround for some routing actions which return collections or maps containing generic types

--- src/main/java/com/musala/atmosphere/server/websocket/ClientServerWebSocketEndpoint.java
+++ src/main/java/com/musala/atmosphere/server/websocket/ClientServerWebSocketEndpoint.java
@@ -1,10 +1,13 @@
 package com.musala.atmosphere.server.websocket;
 
+import java.lang.reflect.Type;
 import java.rmi.AccessException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
@@ -27,7 +30,9 @@ import com.musala.atmosphere.commons.cs.exception.InvalidPasskeyException;
 import com.musala.atmosphere.commons.cs.exception.NoDeviceMatchingTheGivenSelectorException;
 import com.musala.atmosphere.commons.exceptions.CommandFailedException;
 import com.musala.atmosphere.commons.exceptions.NoAvailableDeviceFoundException;
+import com.musala.atmosphere.commons.ui.tree.AccessibilityElement;
 import com.musala.atmosphere.commons.util.Pair;
+import com.musala.atmosphere.commons.util.structure.tree.Tree;
 import com.musala.atmosphere.commons.websocket.message.ClientServerRequest;
 import com.musala.atmosphere.commons.websocket.message.ClientServerResponse;
 import com.musala.atmosphere.commons.websocket.message.MessageType;
@@ -37,6 +42,8 @@ public class ClientServerWebSocketEndpoint {
     private static final Logger LOGGER = Logger.getLogger(ClientServerWebSocketEndpoint.class.getCanonicalName());
 
     private ClientServerWebSocketCommunicator websocketCommunicator = ClientServerWebSocketCommunicator.getInstance();
+
+    private RoutingAction routingAction;
 
     private Gson gson = new GsonBuilder().serializeNulls().create();
 
@@ -53,10 +60,10 @@ public class ClientServerWebSocketEndpoint {
                 LOGGER.info("Received device allocation request.");
                 DeviceSelector deviceSelector = gson.fromJson(request.getData(), DeviceSelector.class);
                 try {
-                DeviceAllocationInformation deviceAllocationInformation = websocketCommunicator.getDeviceAllocationInformation(deviceSelector);
+                    DeviceAllocationInformation deviceAllocationInformation = websocketCommunicator.getDeviceAllocationInformation(deviceSelector);
 
-                String responseData = gson.toJson(deviceAllocationInformation, DeviceAllocationInformation.class);
-                response = new ClientServerResponse(sessionId, messageType, responseData);
+                    String responseData = gson.toJson(deviceAllocationInformation, DeviceAllocationInformation.class);
+                    response = new ClientServerResponse(sessionId, messageType, responseData);
                 } catch (NoAvailableDeviceFoundException e) {
                     response = new ClientServerResponse(sessionId, messageType, null);
                 } catch (NoDeviceMatchingTheGivenSelectorException e) {
@@ -87,22 +94,29 @@ public class ClientServerWebSocketEndpoint {
                     if (result == null) {
                         // The action ran successfully on the agent, but it did not return a result
                         response = new ClientServerResponse(sessionId, messageType, null);
+                        LOGGER.info("Response is null.");
                     } else {
+                        LOGGER.info("Response class: " + result.getClass().getName());
                         // Put the action result and its class in the response, so we can deserialize it
-                        // correctly on the other end when type erasure occurs. This should be probably
-                        // done on the agent when we fully migrate the communication
+                        // correctly on the other end when type erasure occurs. This should probably be
+                        // done on the agent when we fully migrate the communication.
+                        // serializeCorrectlyIfCollection is a workaround for the cases when the response is a collection.
+                        // Generic types do not play well with Gson serialization and deserialization:
+                        // https://sites.google.com/site/gson/gson-user-guide#TOC-Serializing-and-Deserializing-Generic-Types
                         JsonObject jsonResultWithClass = new JsonObject();
                         jsonResultWithClass.addProperty("class", result.getClass().getName());
-                        jsonResultWithClass.addProperty("value", gson.toJson(result, result.getClass()));
+                        jsonResultWithClass.addProperty("value", gson.toJson(result, serializeCorrectlyIfCollection(result)));
+                        LOGGER.info(jsonResultWithClass.toString());
                         response = new ClientServerResponse(sessionId, messageType, jsonResultWithClass.toString());
                     }
                 } catch (RemoteException | NotBoundException e) { // this block should be gone when we fully migrate to WebSockets
                     response = new ClientServerResponse(sessionId, MessageType.ERROR, e.getMessage());
-                } catch (CommandFailedException | InvalidPasskeyException | DeviceNotFoundException e) {
+                } catch (CommandFailedException | InvalidPasskeyException | DeviceNotFoundException | ClassNotFoundException e) {
                     response = new ClientServerResponse(sessionId, MessageType.ERROR, e.getMessage());
                 }
                 break;
             default:
+                LOGGER.error("The request was of unexpected type.");
                 break;
         }
 
@@ -111,24 +125,52 @@ public class ClientServerWebSocketEndpoint {
 
     private Object sendRoutingAction(String requestData)
             throws AccessException, RemoteException, NotBoundException,
-            CommandFailedException, InvalidPasskeyException, DeviceNotFoundException {
+            CommandFailedException, InvalidPasskeyException, DeviceNotFoundException, ClassNotFoundException {
         JsonObject json = new JsonParser().parse(requestData).getAsJsonObject();
 
         String deviceRmiId = json.get("rmiId").getAsString();
         long passkey = json.get("passkey").getAsLong();
         RoutingAction action = gson.fromJson(json.get("action").getAsString(), RoutingAction.class);
+        this.routingAction = action; // Used with the workaround
+        LOGGER.info("Routing action:" + action);
 
-        JsonArray argsArray = json.get("args").getAsJsonArray();
         List<Object> argsList = new ArrayList<Object>();
+        JsonArray argsArray = json.get("args").getAsJsonArray();
         for (JsonElement jsonArg : argsArray) {
-            Object argObject = gson.fromJson(jsonArg, Object.class);
-            LOGGER.info("Adding arg object: " + argObject.toString());
+            JsonObject jsonObject = jsonArg.getAsJsonObject();
+            Class<?> objectClass = Class.forName(jsonObject.get("class").getAsString());
+            Object argObject = gson.fromJson(jsonObject.get("value").getAsString(), objectClass);
             argsList.add(argObject);
+            LOGGER.info("Adding argument of type " + argObject.getClass().getName() + ": " + argObject.toString());
         }
 
         Object[] argsObjectArray = argsList.toArray();
-        LOGGER.info("Total number of arguments: " + argsObjectArray.length);
 
         return websocketCommunicator.routeAction(deviceRmiId, passkey, action, argsObjectArray);
+    }
+
+    private Type serializeCorrectlyIfCollection(Object object) {
+        Type objectType = null;
+        if (routingAction == RoutingAction.EXECUTE_SHELL_COMMAND_SEQUENCE) {
+            objectType = new TypeToken<List<String>>(){}.getType();
+        } else if (routingAction == RoutingAction.GET_UI_TREE) {
+            objectType = new TypeToken<Tree<AccessibilityElement>>(){}.getType();
+        } else if (routingAction == RoutingAction.GET_UI_ELEMENTS ||
+                routingAction == RoutingAction.GET_CHILDREN ||
+                routingAction == RoutingAction.EXECUTE_XPATH_QUERY_ON_LOCAL_ROOT ||
+                routingAction == RoutingAction.EXECUTE_XPATH_QUERY) {
+            objectType = new TypeToken<List<AccessibilityElement>>(){}.getType();
+        } else if (routingAction == RoutingAction.GET_LOGCAT_BUFFER) {
+            objectType = new TypeToken<List<Pair<Integer, String>>>(){}.getType();
+        } else if (routingAction == RoutingAction.GET_WEB_VIEWS) {
+            objectType = new TypeToken<Set<String>>(){}.getType();
+        } else if (routingAction == RoutingAction.FIND_WEB_ELEMENT) {
+            objectType = new TypeToken<Map<String, Object>>(){}.getType();
+        } else if (routingAction == RoutingAction.FIND_WEB_ELEMENTS) {
+            objectType = new TypeToken<List<Map<String, Object>>>(){}.getType();
+        } else {
+            return object.getClass();
+        }
+        return objectType;
     }
 }
